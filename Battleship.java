0726d2b80dd88165bf0e4f12174e6b404ba3ea4d
Marco <marcoasedano@gmail.com>
2016-07-02 17:29:58 -0700

    Created class stubs that will be used in the Battleship game

---
+++ Battleship.java
@@ -0,0 +1,13 @@
+/**
+ * Represents a battleship that could be hit during gameplay.
+ *
+ * Bugs: none known
+ *
+ * @author       Marco Sedano
+ * @version      1.0
+ * 
+ */
+
+ public class Battleship {
+ 
+ }
---
+++ BattleshipGame.java
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   
+// Main Class File:  BattleshipGame.java
+//
+// Main class used to run and start the Battleship game.
+// 
+//
+// Author:           Marco Sedano
+//
+// Last Date Modified: 07/02/2016
+//
+///////////////////////////////////////////////////////////////////////////////
+
+public class BattleshipGame.java {
+	public static void main(String[] args) {
+		System.out.println("Lets play some Battleship!");
+	}
+}
