cc3ff8901228e8fbb85a195b4252388473bcb9dd
Hector Delgado <Hector.d.delgado@gmail.com>
2016-07-05 14:07:47 -0700

    Initializing Game board
    Creating game board

--- BattleshipGame.java
+++ BattleshipGame.java
@@ -11,13 +11,29 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 import javax.swing.*;
-public class BattleshipGame.java {
-	public static void main(String[] args) {
+import java.util.Random;
+
+public class BattleshipGame.java 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Lets play some Battleship!");
 		JOptionPane.showMessageDialog(
 			null, 
 			"I Want to play a game...", 
 			"Lets Begin",
 			JOptionPane.PLAIN_MESSAGE);
-	}
-}
+		initBoard();
+
+	}//end method main
+
+	//initialize playing field as 5x5 board
+	public static void initBoard()
+	{
+		//size of board
+		final int ROWS = 5;
+		final int COLUMNS = 5;
+
+		int board[][] = new int[ROWS][COLUMNS];
+	}//end method initBoard
+}//end class BattleshipGame
