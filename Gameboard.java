edb0784ffbec2a4361c023798aae88f13703b643
Marco <marcoasedano@gmail.com>
2016-07-05 20:17:35 -0700

    Fixed syntax errors in the gamboard class

--- Gameboard.java
+++ Gameboard.java
@@ -12,33 +12,39 @@
 
  public class Gameboard {
 
- 	private static FINAL int BOARD_SIZE = 100;
- 	private static FINAL int ROWS = 10;
- 	private static FINAL int COLUMNS = 10;
- 	private int board[COLUMNS][ROWS];
+ 	private static final int BOARD_SIZE = 100;
+ 	private static final int ROWS = 10;
+ 	private static final int COLUMNS = 10;
+ 	private int board[][];
 
  	// initialize a game board with all of it's values equal to 0
  	// 1 means a hit
  	// 0 means empty (not touched yet)
  	// -1 means a miss
  	public Gameboard() {
- 		board = {0};
+ 		board = new int[COLUMNS][ROWS];
+
+ 		for (int i = 0; i < COLUMNS; i++) {
+ 			for (int j = 0; j < ROWS; j++) {
+ 				board[i][j] = 0;
+ 			}
+ 		}
  	}
 
  	// returns the status of space located at column x row
  	// throws exception if column/row specified are out of bounds
  	public int getStatus(int column, int row) throws Exception {
- 		if (column > 10 || column < 1) && (row > 10 || row < 1) {
- 			throw Exception("Your column and row entered are out of bounds!");
+ 		if ((column > 10 || column < 1) && (row > 10 || row < 1)) {
+ 			throw new Exception("Your column and row entered are out of bounds!");
  		}
- 		else if (column > 10 || column < 1) {
- 			throw Exception("Your column is out of bounds!");
+ 		else if ((column > 10 || column < 1)) {
+ 			throw new Exception("Your column is out of bounds!");
  		}
- 		else if (row > 10 || row < 1) {
- 			throw Exception("Your row is out of bounds!");
+ 		else if ((row > 10 || row < 1)) {
+ 			throw new Exception("Your row is out of bounds!");
  		}
 
- 		returns board[column][row];
+ 		return board[column][row];
  	}
 
  	// returns the associated column number (ex. A = 0, B = 1, etc)
