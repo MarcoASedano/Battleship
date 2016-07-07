4614ae859b53c12c6c1bc1f2d0e9b9fa52ede424
Hector Delgado <Hector.d.delgado@gmail.com>
2016-07-07 15:49:45 -0700

    Removed pop-up message

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -10,18 +10,12 @@
 // Last Date Modified: 07/02/2016
 //
 ///////////////////////////////////////////////////////////////////////////////
-import javax.swing.*;
 import java.util.Random;
 
 public class BattleshipGame {
 	public static void main(String[] args) {
 		System.out.println("Lets play some Battleship!");
-		JOptionPane.showMessageDialog(
-			null, 
-			"I Want to play a game...", 
-			"Lets Begin",
-			JOptionPane.PLAIN_MESSAGE);
-        
+
         //TEST CODE>>>>DELETE THIS WHEN DONE
         //
         //
@@ -29,8 +23,7 @@ public class BattleshipGame {
         newBoard.showBoard();
         //
         //
-        //////////////////////////////////////
-        
+        //////////////////////////////////////     
         
 	}//end method main
 }//end class BattleshipGame
