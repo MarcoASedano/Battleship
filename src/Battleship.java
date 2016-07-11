7236c36915cf173cbcfa03336cf25c63c7584776
Hector Delgado <Hector.d.delgado@gmail.com>
2016-07-10 21:35:23 -0700

    Added method to set ship location

--- src/Battleship.java
+++ src/Battleship.java
@@ -7,7 +7,7 @@
  * @version      1.0
  * 
  */
-
+import java.util.Scanner;
  public class Battleship {
  
  	private int size;
@@ -56,14 +56,96 @@
 				break;
 			default:
 				System.out.println("Invalid ship type!");
- 		}
- 	}
-
+ 		}//end switch
+ 	}//end constructor
 
  	// sets the location for a ship
  	// takes a column (already converted to a number) and a row
  	// checks have to be made to make sure imputed 
- 	public void setShipLocation(int row, int column) {
+ 	public void setShipLocation(char column, int row) {
+        boolean columnValid = false;
+        boolean rowValid = false;
+        boolean doesExist = false;
+        int adjustedColumn = 0;    
+        int adjustedRow = 0;
+        int counter = 0;
+        
+        try {
+            //try to set ship column
+            char columnToUpper = Character.toUpperCase(column);
+            if(columnToUpper >= 'A' && columnToUpper <= 'J') {
+                System.out.println("Column is Valid!");
+                for(char letter = 'A'; letter <= 'J'; letter++) {
+                    if(columnToUpper == letter) {
+                        adjustedColumn = counter;
+                        break;
+                    }//end if
+                    else {
+                        counter++;
+                    }//end else
+                }//end for loop
+                rowValid = true;
+            }//end if
+            else {
+                while(columnValid == false) {
+                    //prompt user for new column
+                    System.out.println("Column not recognized! Please enter a "+
+                                       "letter between A and J");
+                    Scanner userInput = new Scanner(System.in);
+                    char newInput = userInput.next().charAt(0);
 
- 	}
- }
+                    //re-evaluate input
+                    columnToUpper = Character.toUpperCase(newInput);
+                    if(columnToUpper >= 'A' && columnToUpper <= 'J') {
+                        System.out.println("Column is Valid");
+                        for(char letter = 'A'; letter <= 'J'; letter++) {
+                            if(columnToUpper == letter) {
+                                adjustedColumn = counter;
+                                break;
+                            }//end if
+                            else {
+                                counter++;
+                            }//end else
+                        }//end for loop
+                        columnValid = true;
+                    }//end if
+                }//end while
+            }//end else
+
+            //try to set ship row
+            adjustedRow = row - 1;
+            if(adjustedRow >= 0 && adjustedRow <= 9) {
+                System.out.println("Row is Valid!");
+                rowValid = true;
+            }//end if   
+            else
+            {
+                while(rowValid == false)
+                {
+                    //prompt user for new row
+                    System.out.println("Row not recognized! Please enter a "+
+                                       "number between 1 and 10");
+                    Scanner userInput = new Scanner(System.in);
+                    int newInput = userInput.nextInt();
+
+                    //re-validate input
+                    adjustedRow = newInput - 1;
+                    if(adjustedRow >= 0 && adjustedRow <= 9) {
+                        System.out.println("Row is Valid!");
+                        rowValid = true;
+                    }//end if   
+                }//end while
+            }//end else
+
+            location[adjustedColumn][adjustedRow] = 0;
+            System.out.println("Ship created at location " + column + "-" + row);
+        }//end try
+        catch(ArrayIndexOutOfBoundsException ex) {
+            System.out.println("ArrayIndexOutOfBoundsException Caught!");
+        }
+        catch(NullPointerException ex)
+        {
+            System.out.println("NullPointerException Caught");
+        }
+ 	}//end method setShipLocation
+ }//end class Battleship
--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -19,8 +19,17 @@ public class BattleshipGame {
         //TEST CODE>>>>DELETE THIS WHEN DONE
         //
         //
+        
         Gameboard newBoard = new Gameboard();
         newBoard.showBoard();
+        
+        System.out.println("\nCreating ship1");
+        Battleship ship1 = new Battleship(3);
+        ship1.setShipLocation('C', 3);
+        
+        System.out.println("\nCreating ship2");
+        Battleship ship2 = new Battleship(50);
+        ship2.setShipLocation('X', 22);
         //
         //
         //////////////////////////////////////     
